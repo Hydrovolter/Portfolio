f293d1e3c58768f7c6e7bbe13b51fd89063fa3d5
Hydrovolter <rionray1234@gmail.com>
2025-05-23 21:50:42 +0100

    add registerInterval to kana theme

--- games/js/themes/kana.js
+++ games/js/themes/kana.js
@@ -105,6 +105,7 @@ export function kanaTheme() {
       canvas.height = window.innerHeight;
       init();
     }, 250);
+    registerInterval(resizeTimeout);
   }
 
   registerEventListener(window, "mousemove", handleMouseMove);
